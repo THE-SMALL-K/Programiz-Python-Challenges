324956d7a8063df669745686078049e80215c7f2
lightningfast <khoi304857@gmail.com>
2025-03-25 15:36:10 -0700

    add and substract

--- Hard/ComplexNumOp.py
+++ Hard/ComplexNumOp.py
@@ -1,4 +1,3 @@
-''''
 def complex_operations(num1, num2):
     list1 = []
     for i in range(len(num1)):
@@ -6,7 +5,7 @@ def complex_operations(num1, num2):
     list1.remove("+")
     
     real1 = int(list1[0])
-    immaginiary1 = int(list1[1])
+    imaginary1 = int(list1[1])
 
     list2 = []
     for i in range(len(num2)):
@@ -14,32 +13,39 @@ def complex_operations(num1, num2):
     list2.remove("+")
 
     real2 = int(list2[0])
-    immaginiary2 = int(list2[1])
+    imaginary2 = int(list2[1])
 
-    Addition(real1,immaginiary1,real2,immaginiary2)
-    Subtract(real1,immaginiary1,real2,immaginiary2)
-    Multiply(real1,immaginiary1,real2,immaginiary2)
-    Divide(real1,immaginiary1,real2,immaginiary2)
-    
+    a = str(Addition_real(real1,real2))
+    b = str(Addition_im(imaginary1,imaginary2))
 
-def Addition(real_num1, im_num1,real_num2, im_num2):
+    c = str(Subtract_real(real1,real2))
+    d = str(Substract_im(imaginary1,imaginary2))
+
+    #Multiply(real1,imaginary1,real2,imaginary2)
+    #Divide(real1,imaginary1,real2,imaginary2)
+    return "{Addition:" + "(" + a + "+" + b + "j), Subtraction:" + "(" + c + "+" + d + "j)}"
+
+def Addition_real(real_num1, real_num2):
     sum_real = real_num1 + real_num2
+    return sum_real
+
+def Addition_im(im_num1,im_num2):
     sum_im = im_num1 + im_num2
-    print(sum_real)
-    print(sum_im)
+    return sum_im
 
-def Subtract(real_num1, im_num1,real_num2, im_num2):
+def Subtract_real(real_num1,real_num2):
     diff_real = real_num1 - real_num2
+    return diff_real
+def Substract_im(im_num1, im_num2):
     diff_im = im_num1 - im_num2
-    print(diff_real)
-    print(diff_im)
-
+    return diff_im
+'''
 def Multiply(real_num1, im_num1,real_num2, im_num2):
     multi_real = real_num1 - real_num2
     multi_im = im_num1 - im_num2
+
 def Divide(real_num1, im_num1,real_num2, im_num2):
     divide_real = real_num1 - real_num2
     divide_im = im_num1 - im_num2    
-
-print(complex_operations("2+3j", "1+2j"))
 '''
+print(complex_operations("2+3j", "1+2j"))
